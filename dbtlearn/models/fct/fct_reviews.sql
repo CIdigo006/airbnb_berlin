a0828db39d9563755a543cc67a06a0effef33439
Chu-Chu Idigo <cidigo97@gmail.com>
2023-11-06 12:26:26 -0500

    added all my files up till this point

---
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized = "incremental",
+        on_schema_change = 'fail'
+    )
+}}
+
+with src_reviews AS (
+    SELECT
+        *
+    FROM
+        {{ ref("src_reviews") }}
+)
+
+SELECT 
+    * 
+FROM 
+    src_reviews
+WHERE
+    review_comments is not null
+{% if is_incremental() %}
+  and review_date > (select max(review_date) from {{ this }})
+{% endif %}
